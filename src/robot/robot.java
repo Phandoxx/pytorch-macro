5373486aaed33f22404d6b6c891ac1c4b974d03f
Phandoxx <143786592+Phandoxx@users.noreply.github.com>
2025-10-07 08:18:47 +1300

    first

---
+++ src/robot/robot.java
@@ -0,0 +1,44 @@
+package robot;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+
+public class robot {
+
+    static Robot bot; // class-level robot
+    static int openInvKey = KeyEvent.VK_E;
+
+    public static void main(String[] args) throws AWTException {
+        bot = new Robot(); // initialize the class-level bot
+        attack("knockback mode");
+    }
+
+    public static void attack(String weapon) {
+        if ("knockback mode".equalsIgnoreCase(weapon)) {
+            knockbackMode();
+        }
+        else {
+            int cooldown = switch (weapon) {
+                case "sword" -> 650;
+                case "axe" -> 1000;
+                default -> 0;
+            };
+            bot.delay(cooldown);
+
+            bot.mousePress(MouseEvent.BUTTON1_MASK);
+            bot.mouseRelease(MouseEvent.BUTTON1_MASK);
+        }
+
+    }
+
+    public static void openInv() {
+        bot.keyPress(openInvKey);
+        bot.keyRelease(openInvKey);
+    }
+
+    public static void knockbackMode() {
+        System.out.println("Knockback mode active! Tracking player...");
+    }
+}
