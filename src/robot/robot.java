9d99754d0c7719da33532089d54c99137bca48de
Phandoxx <143786592+Phandoxx@users.noreply.github.com>
2025-10-07 19:25:38 +1300

    axe tier logic complete

--- src/robot/robot.java
+++ src/robot/robot.java
@@ -12,22 +12,30 @@ public class robot {
 
     public static void main(String[] args) throws AWTException {
         bot = new Robot(); // initialize the class-level bot
-        attack("knockback mode", "stone");
+        attack("knockback mode", "wooden-stone-copper");
         openInv();
     }
 
-    public static void attack(String weapon, String teir) {
+    public static void attack(String weapon, String tier) {
         if ("knockback mode".equalsIgnoreCase(weapon)) {
             knockbackMode();
         }
         else {
             int cooldown = switch (weapon) {
+                case "spear" -> 1;
                 case "sword" -> 650;
-                case "axe" -> 1000;
                 default -> 0;
             };
 
             //tier handling logic goes here, check if weapon = axe then add to cooldown based on tier
+            if (weapon.equals("axe")) {
+                cooldown = switch (tier) {
+                    case "wooden-stone-copper" -> 1250;
+                    case "iron" -> 1110;
+                    case "gold-diamond-netherite" -> 1000;
+                    default -> 0;
+                };
+            }
 
             bot.delay(cooldown);
 
