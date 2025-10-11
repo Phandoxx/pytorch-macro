71583633e3d37b82e76039293121d32d2a515a6d
Phandoxx <143786592+Phandoxx@users.noreply.github.com>
2025-10-11 20:33:47 +1300

    threading setup for teams bot

--- src/robot/teams.java
+++ src/robot/teams.java
@@ -1,9 +1,41 @@
 package robot;
 
 public class teams {
-    public static void teamsCallLoop() {
-        System.out.println("teamsCallLoop Activated");
 
+    // First method
+    public static void mainCallThread() {
+        robot.findTemplateOnScreen("images/spam/teams/teams_chat_button_disabled.png", "click", "true");
     }
 
+    // Second method
+    public static void watchdog() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("watchdog running: " + i);
+            try { Thread.sleep(700); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+    }
+
+    public static void teamsCallLoop() {
+        // Thread for mainCallThread
+        Thread t1 = new Thread() {
+            public void run() {
+                mainCallThread();
+            }
+        };
+
+        // Thread for watchdog
+        Thread t2 = new Thread() {
+            public void run() {
+                watchdog();
+            }
+        };
+
+        System.out.println("teamsCallLoop Activated");
+        t1.start();
+        t2.start();
+    }
+
+    public static void main(String[] args) {
+        teamsCallLoop();
+    }
 }
